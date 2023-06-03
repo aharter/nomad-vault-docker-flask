4e4e02ce94f1fcc8eaa12c574cb0dae6136ab4d8
nasenblick <info@nasenblick.de>
2023-06-04 00:17:20 +0200

    removing private key

--- main.tf
+++ main.tf
@@ -180,7 +180,7 @@ resource "tls_private_key" "private_key" {
 resource "aws_key_pair" "generated_key" {
   key_name   = "tf-key"
   public_key = var.aws_ssh_public_key
-  private_key = var.aws_ssh_private_key
+  #private_key = var.aws_ssh_private_key
   #public_key = tls_private_key.private_key.public_key_openssh
 }
 

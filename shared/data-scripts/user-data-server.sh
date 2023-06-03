a6de47b4e337501ec1fb2ea38d604e15371c2168
nasenblick <info@nasenblick.de>
2023-06-03 15:13:50 +0200

    moved consult-template to existing .sh scripts

--- shared/data-scripts/user-data-server.sh
+++ shared/data-scripts/user-data-server.sh
@@ -4,6 +4,14 @@ set -e
 
 exec > >(sudo tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
 
+# Download and install Consul Template
+CONSUL_TEMPLATE_VERSION="0.32.0"
+CONSUL_TEMPLATE_DOWNLOAD="https://releases.hashicorp.com/consul-template/${CONSUL_TEMPLATE_VERSION}/consul-template_${CONSUL_TEMPLATE_VERSION}_linux_amd64.zip"
+curl -L ${CONSUL_TEMPLATE_DOWNLOAD} > consul-template.zip
+sudo unzip consul-template.zip -d /usr/local/bin
+sudo chmod 0755 /usr/local/bin/consul-template
+sudo chown root:root /usr/local/bin/consul-template
+
 ACL_DIRECTORY="/ops/shared/config"
 NOMAD_BOOTSTRAP_TOKEN="/tmp/nomad_bootstrap"
 NOMAD_USER_TOKEN="/tmp/nomad_user_token"
--- shared/data-scripts/user-data-vault.sh
+++ shared/data-scripts/user-data-vault.sh
@@ -4,13 +4,20 @@ set -e
 
 exec > >(sudo tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
 
-VAULT_VERSION="1.13.2"
-VAULT_DOWNLOAD="https://releases.hashicorp.com/vault/${VAULT_VERSION}/vault_${VAULT_VERSION}_linux_amd64.zip"
+# Download and install Consul Template
+CONSUL_TEMPLATE_VERSION="0.32.0"
+CONSUL_TEMPLATE_DOWNLOAD="https://releases.hashicorp.com/consul-template/${CONSUL_TEMPLATE_VERSION}/consul-template_${CONSUL_TEMPLATE_VERSION}_linux_amd64.zip"
+curl -L ${CONSUL_TEMPLATE_DOWNLOAD} > consul-template.zip
+sudo unzip consul-template.zip -d /usr/local/bin
+sudo chmod 0755 /usr/local/bin/consul-template
+sudo chown root:root /usr/local/bin/consul-template
 
 # Install jq
 sudo apt-get install jq
 
 # Install Vault
+VAULT_VERSION="1.13.2"
+VAULT_DOWNLOAD="https://releases.hashicorp.com/vault/${VAULT_VERSION}/vault_${VAULT_VERSION}_linux_amd64.zip"
 curl -L ${VAULT_DOWNLOAD} > vault.zip
 sudo unzip vault.zip -d /usr/local/bin
 sudo chmod 0755 /usr/local/bin/vault

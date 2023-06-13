b8a20f0f94188e11f6444e226c3b092a68a2cd4e
nasenblick <info@nasenblick.de>
2023-06-13 14:56:08 +0200

    switching to apt-get

--- shared/data-scripts/user-data-vault.sh
+++ shared/data-scripts/user-data-vault.sh
@@ -19,10 +19,10 @@ sudo snap install jq
 
 
 # Install Vault
-sudo curl -L https://releases.hashicorp.com/vault/1.13.3/vault_1.13.3_darwin_amd64.zip > vault.zip
-sudo unzip vault.zip -d /usr/bin
-sudo chmod 0755 /usr/bin/vault
-sudo chown root:root /usr/bin/vault
+echo "Starting Vault Install"
+wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update && sudo apt install vault
 
 sed -i "s|IP_ADDRESS|$IP_ADDRESS|g" $CONFIGDIR/vault.hcl
 sudo mkdir $VAULTCONFIGDIR
@@ -30,7 +30,6 @@ sudo chmod 0755 $VAULTCONFIGDIR
 sudo cp $CONFIGDIR/vault.hcl $VAULTCONFIGDIR/vault.hcl
 sudo mkdir -p /usr/lib/systemd/system
 sudo cp $CONFIGDIR/vault.service /usr/lib/systemd/system/vault.service
-echo "Concluded Vault Preparation"
 
 sudo systemctl enable vault
 sudo systemctl restart vault
@@ -38,11 +37,13 @@ sudo systemctl restart vault
 echo "Vault started"
 
 # Initialize Vault and retrieve the initial root token
-# sudo vault operator init -key-shares=1 -key-threshold=1 > /tmp/vault_init_output
-# export VAULT_TOKEN=$(grep "Initial Root Token:" /tmp/vault_init_output | awk '{print $NF}')
+sudo vault operator init -key-shares=1 -key-threshold=1 > /tmp/vault_init_output
+export VAULT_TOKEN=$(grep "Initial Root Token:" /tmp/vault_init_output | awk '{print $NF}')
 
 # Store the root token securely (you can modify this as per your requirements)
-# echo "VAULT_ROOT_TOKEN=${VAULT_TOKEN}" | sudo tee /etc/vault/root_token
+echo "VAULT_ROOT_TOKEN=${VAULT_TOKEN}" | sudo tee /etc/vault/root_token
 
 # # Clean up temporary files
-# rm /tmp/vault_init_output
+rm /tmp/vault_init_output
+
+echo "Vault setup concluded"

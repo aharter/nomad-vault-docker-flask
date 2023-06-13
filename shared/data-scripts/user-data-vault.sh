47519188e695aacfe5ce79cf615ffd1ae958d96e
nasenblick <info@nasenblick.de>
2023-06-13 15:48:10 +0200

    misc fixes

--- shared/data-scripts/user-data-vault.sh
+++ shared/data-scripts/user-data-vault.sh
@@ -7,7 +7,6 @@ exec > >(sudo tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 
 CONFIGDIR="/ops/shared/config"
 VAULTCONFIGDIR="/etc/vault.d"
 IP_ADDRESS=$(curl http://instance-data/latest/meta-data/local-ipv4)
-IP_ADDRESS_PUBLIC=$(curl http://instance-data/latest/meta-data/public-ipv4)
 
 # Prepare instance
 sudo apt update
@@ -26,7 +25,6 @@ echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://
 sudo apt update && sudo apt install vault
 
 sed -i "s|IP_ADDRESS|$IP_ADDRESS|g" $CONFIGDIR/vault.hcl
-sed -i "s|IP_ADDRESS_PUBLIC|$IP_ADDRESS_PUBLIC|g" $CONFIGDIR/vault.hcl
 # sudo mkdir $VAULTCONFIGDIR
 # sudo chmod 0755 $VAULTCONFIGDIR
 sudo cp $CONFIGDIR/vault.hcl $VAULTCONFIGDIR/vault.hcl
@@ -39,13 +37,13 @@ sudo systemctl restart vault
 echo "Vault started"
 
 # Initialize Vault and retrieve the initial root token
-sudo vault operator init -key-shares=1 -key-threshold=1 > /tmp/vault_init_output
-export VAULT_TOKEN=$(grep "Initial Root Token:" /tmp/vault_init_output | awk '{print $NF}')
+# sudo vault operator init -key-shares=1 -key-threshold=1 > /tmp/vault_init_output
+# export VAULT_TOKEN=$(grep "Initial Root Token:" /tmp/vault_init_output | awk '{print $NF}')
 
-# Store the root token securely (you can modify this as per your requirements)
-echo "VAULT_ROOT_TOKEN=${VAULT_TOKEN}" | sudo tee /etc/vault/root_token
+# # Store the root token securely (you can modify this as per your requirements)
+# echo "VAULT_ROOT_TOKEN=${VAULT_TOKEN}" | sudo tee /etc/vault/root_token
 
-# # Clean up temporary files
-rm /tmp/vault_init_output
+# Clean up temporary files
+# rm /tmp/vault_init_output
 
-echo "Vault setup concluded"
+# echo "Vault setup concluded"
